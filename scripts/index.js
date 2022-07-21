d3f4cb6b43c6f680182b5aeab72f5aa3a29665fc
D.M.S <thebest755p@gmail.com>
2022-07-21 03:10:58 +0200

    Additional info about comments added

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,15 @@ window.onload = function () {
     const flowField = new FlowField(ctx, canvas.width, canvas.height);
 }
 
+// My comments use the Better Comments extension by Aaron Bond, hence the strange symbols (they make the comment have fancy colors)
+
+/*
+Basically comment with the prefix of:
+? - is additional information
+NOTE - are facts and important mentions
+! - are very important things (keep an eye on them!)
+*/
+
 // First off, I'll be trying to implement a flow field-like effect
 // I'm using object oriented approach, so don't get confused if you don't understand something!
 // I will try to explain some things along the way
@@ -16,6 +25,8 @@ class FlowField {
     #width;
     #height;
     // Here, the "this." references create the encapsulation as the constructor will get the global values and convert them to the private class fields
+    // ? Encapsulation is the bundling of data with the methods that work on that data into a single unit
+    
     // NOTE: Technically you could be referencing for example the ctx value from line 3 directly in the draw function, but it is considered a bad practice and we should avoid that
     constructor(ctx, width, height) {
         this.#ctx = ctx;
