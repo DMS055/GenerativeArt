434fd9ac9fdd382843215dabd17d87acbbf37711
D.M.S <thebest755p@gmail.com>
2022-07-21 02:22:49 +0200

    Initial commit (setup)

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    const canvas = document.getElementById('canvas-1');     
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;        
+}
